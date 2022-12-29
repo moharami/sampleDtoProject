2447962364adf1e2ca505365736760d74184aa3c
Amir Moharami <a.moharami@gmail.com>
2022-12-29 18:23:46 +0330

    refactor

--- app/Http/Controllers/BankController.php
+++ app/Http/Controllers/BankController.php
@@ -3,13 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Data\getContent;
+use App\Data\Source;
+use App\DataTransferObjects\BankDTO;
+use Illuminate\Http\Request;
 
 class BankController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $jsonData = getContent::fromJsonfile();
-
-
+        $data = Source::getData($request->source);
+        $object = BankDTO::make($data);
+        dd($object);
     }
 }
